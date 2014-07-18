532cf897e4a2a59945ed6ed552a4cd31539fa4b2
Yury Tarabanko <tarabanko@mirapolis.ru>
2014-07-18 18:51:09 +0400

    chai asserts added

--- src/callbackify.js
+++ src/callbackify.js
@@ -1,4 +1,15 @@
 /*jshint node:true*/
 module.exports = function callbackify(cb) {
-
+    return function (err, data, next) {
+        if(err) {
+            return next && next(err);
+        }
+        try {
+            cb(data);
+        }
+        catch (e) {
+            err = e;
+        }
+        return next && next(err, data);
+    };
 };
--- test/test.js
+++ test/test.js
@@ -1,17 +1,65 @@
 /*jshint node:true*/
 /*globals describe, it*/
+/*jshint -W030*/
 
-var t = require('assert');
 var ify = require('../index.js');
+var chai = require('chai');
+
+chai.should();
+var t = chai.expect;
+
 
 describe('ifyify', function() {
     it('should be defined', function() {
-        t.ok(ify);
+        ify.should.be.a('object');
     });
 
     describe('callbackify', function() {
         it('should be defined', function() {
-            t.equal(typeof ify.callbackify, 'function');
+            ify.callbackify.should.be.a('function');
+        });
+
+        var arg = {}, wrapped = ify.callbackify(test);
+        function test(data) {
+            data.should.be.equal(arg);
+        }
+
+        it('should return wrapped function', function() {
+            wrapped.should.be.a('function');
+        });
+
+        it('should run wrapped function with argument specified', function() {
+            wrapped(false, arg);
+        });
+
+        it('should not call function when error is specified', function() {
+            ify.callbackify(function() {
+                t(false).to.be.ok;
+            })({}, {}, function() {
+                t(true).to.be.ok;
+            });
+        });
+
+        it('should call next', function() {
+            var data = {}, i = 0;
+            ify.callbackify(function() {
+                i++;
+                t(true).to.be.true;
+            })(false, data, function (err, arg) {
+                i++;
+                err.should.be.false;
+                arg.should.be.equal(data);
+            });
+
+            i.should.be.equal(2);
+        });
+
+        it('should pass error if function throws', function() {
+            ify.callbackify(function() {
+                throw new Error('Should be passed');
+            })(false, {}, function(err) {
+                err.should.be.ok;
+            });
         });
     });
 });
